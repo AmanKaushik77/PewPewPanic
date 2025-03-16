a42551861e56acf1ff278bcc48a5372337f2c18f
AmanKaushik77 <100389449+AmanKaushik77@users.noreply.github.com>
2025-03-16 16:09:35 -0700

    enemy updated

--- Assets/Scripts/EnemyManger.cs
+++ Assets/Scripts/EnemyManger.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class EnemyManager : MonoBehaviour
 {
     public GameObject enemy;
+    public GameObject bullet;
     public float moveSpeed = 50f;
     public Vector3 moveDirection = Vector3.forward; // Moves enemy to the right
-    public int health = 25; // Initial health of the enemy
+    public int health = 5; // Initial health of the enemy
 
     private void Update()
     {
@@ -15,11 +16,24 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    public void TakeDamage()
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Triggered with: " + other.gameObject.name);
+        if (other.CompareTag("Bullet")) // Check tag instead of name
+        {
+            Debug.Log("Bullet hit! Health before hit: " + health);
+            TakeDamage();
+            Debug.Log("Health after hit: " + health);
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void TakeDamage()
     {
         health--;
         if (health <= 0)
         {
+            Debug.Log("Enemy destroyed!");
             Destroy(enemy);
         }
     }
